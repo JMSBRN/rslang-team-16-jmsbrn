6dc981a84453b77170328d84b8e9ab50cb157b5d
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-09 15:14:59 +0300

    feat: update login and auth components - add inputs checks  and show psw

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,6 +4,15 @@ import { Context } from '../Context'
 
 function Auth() {
 	const {closeAuth, handleGetEmail, handleGetPassword, getRegistrValues} = useContext(Context);
+
+	function showPassword() {
+		var x = document.getElementById("auth-input-psw");
+		if (x.type === "password") {
+			x.type = "text";
+		} else {
+			x.type = "password";
+		}
+	}
 	return (
 		<div>
 				<div id='auth' className='auth-hide'>
@@ -11,11 +20,13 @@ function Auth() {
 		<div className="auth-container">
 			<h4 className='auth-title'>Lorem ipsum dolor sit amet consectetur</h4>
 			<p className='auth-paragraf'>Lorem ipsum dolor sit!</p>
-			<div className="auth-input-container">
-				<input onChange={handleGetEmail} className='auth-input' type="text" placeholder='E-mail'/>
-				<input onChange={handleGetPassword} className='auth-input' type="text" placeholder='Password'/>
+			<form className="auth-input-container">
+				<input onChange={handleGetEmail} className='auth-input' type="email" placeholder='E-mail'/>
+				<input onChange={handleGetPassword}id="auth-input-psw"  className='auth-input' type="password"minLength={8} placeholder='Password' required/>
 				<button onClick={getRegistrValues} className='auth-submit-btn'>Register</button>
-			</div>
+			</form>
+			<input onClick={showPassword} type="checkbox" name="auth-show-psw"/>
+			<span className='auth-checkbox-title'>Show Password</span>
 			<div className="auth-links-container">		
 			</div>
 		</div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,14 @@ import {Context} from '../Context';
 
 function Login() {
 	const {closeLogin,openAuth, handleGetEmail, handleGetPassword, getLoginValues} = useContext(Context);
-
+	function showPassword() {
+		var x = document.getElementById("login-input-psw");
+		if (x.type === "password") {
+			x.type = "text";
+		} else {
+			x.type = "password";
+		}
+	}
 
 	return (
 	<div id='login' className='login-hide'>
@@ -11,11 +18,13 @@ function Login() {
 		<div className="login-container">
 			<h4 className='login-title'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita eos dolorem consectetur amet ducimus.</h4>
 			<p className='login-paragraf'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ipsum numquam voluptate atque, iure voluptatum dicta nisi ratione soluta tempore!</p>
-			<div className="login-input-container">
-				<input onChange={handleGetEmail} className='login-input' type="text" placeholder='E-mail'/>
-				<input onChange={handleGetPassword} className='login-input' type="text" placeholder='Password'/>
+			<form className="login-input-container">
+				<input onChange={handleGetEmail} className='login-input' type="email" placeholder='E-mail'/>
+				<input onChange={handleGetPassword}   id="login-input-psw" className='login-input' type="password"  minLength={8} placeholder='Password' required/>
 				<button onClick={getLoginValues} className='login-submit-btn'>Login</button>
-			</div>
+			</form>
+			<input onClick={showPassword} type="checkbox" name="login-show-psw"/>
+			<span className='login-checkbox-title'>Show Password</span>
 			<div className="login-links-container">
 				<a className='login-link' href="/#">Forgot your passowrd?</a>
 				<a onClick={openAuth} className='login-link' href="/#">Registration</a>
