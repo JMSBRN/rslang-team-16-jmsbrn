689df8a42928c67867e96ddff6abb4a46157cd61
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-17 17:58:52 +0300

    fix: remove teg in word-card-view

--- src/components/WordView.jsx
+++ src/components/WordView.jsx
@@ -20,6 +20,14 @@ function WordView() {
 	const audio = document.getElementById('audioExample');
 	audio.play();
 };
+
+function removeTags(str) {
+	if ((str===null) || (str===''))
+			return false;
+	else
+			str = str.toString();
+	return str.replace( /(<([^>]+)>)/ig, '');
+};
 	return (
 		<div className='word-view'>
 		 {newarr.map( word => 
@@ -30,14 +38,14 @@ function WordView() {
 					 <div className='word-view-transctiption'>{word.transcription}</div>
 					 <button className='word-view-btn' onClick={playAudioWord} >playWord</button>
 					 <audio id='audioWord' src={`https://rslang-team-16-server.herokuapp.com/${word.audio}`}></audio>
-					 <div>{word.textMeaning}</div>
+					 <div>{removeTags(word.textMeaning)}</div>
 					 <button className='word-view-btn' onClick={playAudioMeaning} >playMeaning</button>
 					 <audio id='audioMeaning' src={`https://rslang-team-16-server.herokuapp.com/${word.audioMeaning}`}></audio>
-					 <div className='word-view-text-meaning-translate'>{word.textMeaningTranslate}</div>
-					 <div className='word-view-text-example'>{word.textExample}</div>
+					 <div className='word-view-text-meaning-translate'>{removeTags(word.textMeaningTranslate)}</div>
+					 <div className='word-view-text-example'>{removeTags(word.textExample)}</div>
 					 <button className='word-view-btn' onClick={playAudioExample} >playExample</button>
-					 <audio id='audioExample' src={`https://rslang-team-16-server.herokuapp.com/${word.audioExample}`}></audio>
-					 <div className='word-view-text-example-translate'>{word.textExampleTranslate}</div>
+					 <audio id='audioExample' src={`https://rslang-team-16-server.herokuapp.com/${removeTags(word.audioExample)}`}></audio>
+					 <p className='word-view-text-example-translate'>{removeTags(word.textExampleTranslate)}</p>
 				 </div>)}
 		</div>
 	);
