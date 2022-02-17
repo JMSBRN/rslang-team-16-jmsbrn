55769fb775d066184f2dd9a235287845f7d0e277
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-17 20:17:54 +0300

    fix: menu hide transform position

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,13 +7,13 @@ function Menu() {
 	return (
   	<div id='menu' className="menu-hide">
 			<button onClick={closeMenu} className='menu-btn-close'></button>
-			<div className='menu-nav-links'>	
+			<ul className='menu-nav-links'>	
 				<Link className='menu-link' to={'/'}>Home</Link>
 				<Link className='menu-link' to={'games'}>Games</Link>
 				<Link className='menu-link' to={'about'}>About</Link>
 				<Link className='menu-link' to={'schoolbook'}>Schoolbook</Link>
 				<Link className='menu-link' to={'statistics'}>Statistics</Link>
-			</div>
+			</ul>
 		</div>
 	);
 }
