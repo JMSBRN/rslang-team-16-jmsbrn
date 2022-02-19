c3442deef3431f5e87c7ad1d23ff0c005f493285
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-19 11:30:39 +0300

    feat: update wordView component

--- src/components/WordView.jsx
+++ src/components/WordView.jsx
@@ -1,56 +1,70 @@
-import React, { useContext } from 'react';
-import { Context } from '../Context';
-import '../components/wordView.css';
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import "../components/wordView.css";
 
 function WordView() {
- const {words, wordSchoolbook} = useContext(Context);;
- const newarr = words.filter( word => {
-	 return word.word === wordSchoolbook;
- });
+  const { words, wordSchoolbook } = useContext(Context);
+  const newarr = words.filter((word) => {
+    return word.word === wordSchoolbook;
+  });
 
- const playAudioWord = () => {
-	const audio = document.getElementById('audioWord');
-	audio.play();
-};
- const playAudioMeaning = () => {
-	const audio = document.getElementById('audioMeaning');
-	audio.play();
-};
- const playAudioExample = () => {
-	const audio = document.getElementById('audioExample');
-	audio.play();
-};
-
-function removeTags(str) {
-	if ((str===null) || (str===''))
-			return false;
-	else
-			str = str.toString();
-	return str.replace( /(<([^>]+)>)/ig, '');
-};
-	return (
-		<div className='word-view'>
-		 {newarr.map( word => 
-				 <div key={word.id} className="word-card-view">
-					 <img src= {`https://rslang-team-16-server.herokuapp.com/${word.image}`} alt="word-img" width={280} />
-					 <h2 className='word-view-word'>{word.word}</h2>
-					 <div className='word-view-word-translate'>{word.wordTranslate}</div>
-					 <div className='word-view-transctiption'>{word.transcription}</div>
-					 <button className='word-view-btn' onClick={playAudioWord} >playWord</button>
-					 <audio id='audioWord' src={`https://rslang-team-16-server.herokuapp.com/${word.audio}`}></audio>
-					 <div>{removeTags(word.textMeaning)}</div>
-					 <button className='word-view-btn' onClick={playAudioMeaning} >playMeaning</button>
-					 <audio id='audioMeaning' src={`https://rslang-team-16-server.herokuapp.com/${word.audioMeaning}`}></audio>
-					 <div className='word-view-text-meaning-translate'>{removeTags(word.textMeaningTranslate)}</div>
-					 <div className='word-view-text-example'>{removeTags(word.textExample)}</div>
-					 <button className='word-view-btn' onClick={playAudioExample} >playExample</button>
-					 <audio id='audioExample' src={`https://rslang-team-16-server.herokuapp.com/${removeTags(word.audioExample)}`}></audio>
-					 <p className='word-view-text-example-translate'>{removeTags(word.textExampleTranslate)}</p>
-				 </div>)}
-		</div>
-	);
-};
-export default WordView
-
-
-
+  const playAudio = () => {
+    const audio = document.getElementById("audioWord");
+    audio.play();
+    audio.addEventListener("ended", () => {
+      const audio2 = document.getElementById("audioMeaning");
+      document.getElementById("audioMeaning");
+      audio2.play();
+      audio2.addEventListener("ended", () => {
+        const audio3 = document.getElementById("audioExample");
+        document.getElementById("audioExample");
+        audio3.play();
+      });
+    });
+  };
+  function removeTags(str) {
+    if (str === null || str === "") return false;
+    else str = str.toString();
+    return str.replace(/(<([^>]+)>)/gi, "");
+  }
+  return (
+    <div className="word-view">
+      {newarr.map((word) => (
+        <div key={word.id} className="word-card-view">
+          <img
+            src={`https://rslang-team-16-server.herokuapp.com/${word.image}`}
+            alt="word-img"
+            width={280}
+          />
+          <h2 className="word-view-word">{word.word}</h2>
+          <div className="word-view-word-translate">{word.wordTranslate}</div>
+          <div className="word-view-transctiption">{word.transcription}</div>
+          <button className="word-view-play-btn" onClick={playAudio}></button>
+          <audio
+            id="audioWord"
+            src={`https://rslang-team-16-server.herokuapp.com/${word.audio}`}
+          ></audio>
+          <audio
+            id="audioMeaning"
+            src={`https://rslang-team-16-server.herokuapp.com/${word.audioMeaning}`}
+          ></audio>
+          <audio
+            id="audioExample"
+            src={`https://rslang-team-16-server.herokuapp.com/${word.audioExample}`}
+          ></audio>
+          <p>{removeTags(word.textMeaning)}</p>
+          <div className="word-view-text-meaning-translate">
+            {removeTags(word.textMeaningTranslate)}
+          </div>
+          <p className="word-view-text-example">
+            {removeTags(word.textExample)}
+          </p>
+          <p className="word-view-text-example-translate">
+            {removeTags(word.textExampleTranslate)}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
+export default WordView;
