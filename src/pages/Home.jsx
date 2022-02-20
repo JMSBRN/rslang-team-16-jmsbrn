62cbf5d980558ca5f34bc70d7169e4131c1bd2e4
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-20 16:24:12 +0300

    feat: update home comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import mainPage from '../components/assets/img/main-jpg.webp'
-
+import img from '../components/assets/img/gearl- with-notebook.png'
 const Home = () => {
 	return (
 	<div className='home'>
-    <div className="home-main-title">
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis asperiores expedita animi architecto culpa possimus explicabo temporibus labore aliquam commodi.</h1>
+    <div className="home-title">
+      <h1>Speak with planet</h1>
       <div className="home-description-container">
-			<h3 className='home-description'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ex maxime repudiandae architecto doloribus animi.</h3>
+			<h3 className='home-description'>If you really want to learn english? It's time...</h3>
 			</div>
     </div>
-		<button className='home-video-btn'>Video</button>
-    <div className="home-main-img-container">
-      <img src={mainPage} alt='img.jpg' width="400" height="350"/>
-			<div className='home-nav-links'>
-				<Link className='home-link' to={'/'}>Home</Link>
-				<Link className='home-link' to={'games'}>Games</Link>
-				<Link className='home-link' to={'about'}>About</Link>
-				<Link className='home-link' to={'schoolbook'}>Schoolbook</Link>
-				<Link className='home-link' to={'statistics'}>Statistics</Link>
-			</div>
-    </div>
+		<div className="home-main-container">
+			<img className='home-img' src={img} alt="home-img"/>		
+				<div className='home-nav-links'>
+					<Link className='home-link' to={'/'}>Home</Link>
+					<Link className='home-link' to={'games'}>Games</Link>
+					<Link className='home-link' to={'about'}>About</Link>
+					<Link className='home-link' to={'schoolbook'}>Schoolbook</Link>
+					<Link className='home-link' to={'statistics'}>Statistics</Link>
+				</div>
+		</div>
 	</div>
 	);
 };
