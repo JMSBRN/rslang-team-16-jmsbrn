0a711324ae1dbd57a0a91260d31e57f5016b8ecb
JMSBRN <36186382+JMSBRN@users.noreply.github.com>
2022-02-20 22:10:19 +0300

    feat: update about component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
+
 const About = () => {
 	return (<div className='about'>
+		<div className="about-title">
+We are students! Work hard and hope for the best!!! </div>
 		  <div className="students-cards-container">
 				<div className="students-card">
-					  <img className='student-foto' src="" alt="student-foto" />
+					  <div className='student-foto-one'></div>
 						<div className="studet-work-with-title">
-						  work with 
+						  Dedelops: game Sprint
 						</div>
+						<a className='about-cv-link' href="https://lelek337.github.io/rsschool-cv/">CV</a>
 				</div>
 				<div className="students-card">
-					  <img className='student-foto' src="" alt="student-foto" />
+				<div className='student-foto-two'></div>
 						<div className="studet-work-with-title">
-						  work with 
+						  Develops: game AudioCall 
 						</div>
+						<a className='about-cv-link' href="https://debaev.github.io/rsschool-cv/">CV</a>
 				</div>
 				<div className="students-card">
-					  <img className='student-foto' src="" alt="student-foto" />
+				<div className='student-foto-three'></div>
 						<div className="studet-work-with-title">
-						  work with 
+						  Develops: Shoolbook, frontend, backend, 
 						</div>
+						<a className='about-cv-link' href="https://jmsbrn.github.io/rsschool-cv/">CV</a>
 				</div>
 			</div>
 		</div>);
